c354e9020aed6140b4be85c458b103bb0fef835a
Brenda Lee <brenda.matozo@gmail.com>
2023-10-20 12:43:06 -0300

    chore: add loader component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
-import { RoutesComponent } from './routes/routes.component';
 import { LoaderComponent } from '../shared/loader/loader.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, LoaderComponent, RoutesComponent],
+  imports: [CommonModule, LoaderComponent],
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
