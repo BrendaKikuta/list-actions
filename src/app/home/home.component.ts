483e260c2be2e7f13c85de35fa935b5b62b46809
Brenda Lee <brenda.matozo@gmail.com>
2023-10-20 13:39:18 -0300

    chore: init layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,16 +3,17 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { LoaderComponent } from '../shared/loader/loader.component';
+import { StockExchangesComponent } from '../stock-exchanges/stock-exchanges.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, HeaderComponent, LoaderComponent],
+  imports: [CommonModule, HeaderComponent, LoaderComponent, StockExchangesComponent],
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 
 export class HomeComponent {
-  loading: boolean = true
+  loading: boolean = false
 
   getLoading(): void {
     this.loading = false
--- src/app/stock-exchanges/stock-exchanges.component.ts
+++ src/app/stock-exchanges/stock-exchanges.component.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { ExchangeCardComponent } from './exchange-card/exchange-card.component';
 
 @Component({
   standalone: true,
+  imports: [CommonModule, ExchangeCardComponent],
   selector: 'app-stock-exchanges',
-  templateUrl: './stock-exchanges.component.html'
+  templateUrl: './stock-exchanges.component.html',
+  styleUrls: ['./stock-exchanges.component.scss']
 })
 
-export class StockExchangesComponent {}
+export class StockExchangesComponent {
+  stockExchanges: []
+}
